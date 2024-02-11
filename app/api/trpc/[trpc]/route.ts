084b91359ab52cbbf7835ac12963d3a896ec35b9
santoadji21 <ahmad.aji.santoso21@gmail.com>
2024-02-11 19:27:16 +0700

    fix: route url

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,10 +1,16 @@
 import { appRouter } from '@/app/api/root';
 import { FetchCreateContextFnOptions, fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
+const VERCEL_BASE_URL = process.env.VERCEL_BASE_URL;
+
+if (!VERCEL_BASE_URL) {
+  throw new Error('VERCEL_BASE_URL environment variable is not set');
+}
+
 const handler = (request: Request) => {
   console.log(`incoming request ${request.url}`);
   return fetchRequestHandler({
-    endpoint: '/api/trpc',
+    endpoint: `${VERCEL_BASE_URL}/api/trpc`,
     req: request,
     router: appRouter,
     createContext: function (opts: FetchCreateContextFnOptions): object | Promise<object> {

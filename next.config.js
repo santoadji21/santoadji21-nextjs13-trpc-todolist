dca944c5ccf47cb2a72d0c63a354b68f6163d6ec
santoadji21 <ahmad.aji.santoso21@gmail.com>
2024-02-11 19:38:26 +0700

    fix: disable cors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Accept, Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
